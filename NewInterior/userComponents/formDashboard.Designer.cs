d54a62de5e010de21b2f53d3d22f5c392b8c5dc4
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-01-30 15:59:44 +0600

    added dashboard and manage account

--- NewInterior/userComponents/formDashboard.Designer.cs
+++ NewInterior/userComponents/formDashboard.Designer.cs
@@ -28,20 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(67, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 130);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(385, 102);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(259, 130);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(722, 102);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(259, 130);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(208, 335);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(259, 130);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.panel5.Controls.Add(this.label9);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(590, 335);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(290, 130);
+            this.panel5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Student:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Faculty:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Staff:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Bus:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Booked Seat:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(178, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 23);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "33";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(166, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 23);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "33";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(164, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "33";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(218, 60);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 23);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "33";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(155, 60);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 23);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "33";
             // 
             // formDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(85)))), ((int)(((byte)(58)))));
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "formDashboard";
             this.Size = new System.Drawing.Size(1050, 643);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
     }
 }
--- NewInterior/userComponents/formManageAccount.Designer.cs
+++ NewInterior/userComponents/formManageAccount.Designer.cs
@@ -28,35 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formManageAccount));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addCustomerBtn = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.addVolunteerBtn = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.addEmployeesBtn = new System.Windows.Forms.Label();
+            this.Addpic = new System.Windows.Forms.PictureBox();
+            this.customerRA = new System.Windows.Forms.RadioButton();
+            this.volunteerRA = new System.Windows.Forms.RadioButton();
+            this.employeesRA = new System.Windows.Forms.RadioButton();
+            this.searchBtn = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTB = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Addpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(353, 305);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(244, 33);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Manage Account";
+            this.panel1.Controls.Add(this.addCustomerBtn);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.addVolunteerBtn);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.addEmployeesBtn);
+            this.panel1.Controls.Add(this.Addpic);
+            this.panel1.Controls.Add(this.customerRA);
+            this.panel1.Controls.Add(this.volunteerRA);
+            this.panel1.Controls.Add(this.employeesRA);
+            this.panel1.Controls.Add(this.searchBtn);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.searchTB);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1048, 137);
+            this.panel1.TabIndex = 2;
+            // 
+            // addCustomerBtn
+            // 
+            this.addCustomerBtn.AutoSize = true;
+            this.addCustomerBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCustomerBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.addCustomerBtn.Location = new System.Drawing.Point(849, 99);
+            this.addCustomerBtn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.addCustomerBtn.Name = "addCustomerBtn";
+            this.addCustomerBtn.Size = new System.Drawing.Size(114, 20);
+            this.addCustomerBtn.TabIndex = 26;
+            this.addCustomerBtn.Text = "Add Customer";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(807, 94);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(41, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 25;
+            this.pictureBox3.TabStop = false;
+            // 
+            // addVolunteerBtn
+            // 
+            this.addVolunteerBtn.AutoSize = true;
+            this.addVolunteerBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addVolunteerBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.addVolunteerBtn.Location = new System.Drawing.Point(849, 57);
+            this.addVolunteerBtn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.addVolunteerBtn.Name = "addVolunteerBtn";
+            this.addVolunteerBtn.Size = new System.Drawing.Size(116, 20);
+            this.addVolunteerBtn.TabIndex = 24;
+            this.addVolunteerBtn.Text = "Add Volunteer";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(807, 52);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // addEmployeesBtn
+            // 
+            this.addEmployeesBtn.AutoSize = true;
+            this.addEmployeesBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addEmployeesBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.addEmployeesBtn.Location = new System.Drawing.Point(849, 15);
+            this.addEmployeesBtn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.addEmployeesBtn.Name = "addEmployeesBtn";
+            this.addEmployeesBtn.Size = new System.Drawing.Size(122, 20);
+            this.addEmployeesBtn.TabIndex = 22;
+            this.addEmployeesBtn.Text = "Add Employees";
+            this.addEmployeesBtn.Visible = false;
+            // 
+            // Addpic
+            // 
+            this.Addpic.Image = ((System.Drawing.Image)(resources.GetObject("Addpic.Image")));
+            this.Addpic.Location = new System.Drawing.Point(807, 11);
+            this.Addpic.Margin = new System.Windows.Forms.Padding(2);
+            this.Addpic.Name = "Addpic";
+            this.Addpic.Size = new System.Drawing.Size(41, 31);
+            this.Addpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Addpic.TabIndex = 21;
+            this.Addpic.TabStop = false;
+            this.Addpic.Visible = false;
+            // 
+            // customerRA
+            // 
+            this.customerRA.AutoSize = true;
+            this.customerRA.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerRA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.customerRA.Location = new System.Drawing.Point(296, 104);
+            this.customerRA.Margin = new System.Windows.Forms.Padding(2);
+            this.customerRA.Name = "customerRA";
+            this.customerRA.Size = new System.Drawing.Size(100, 24);
+            this.customerRA.TabIndex = 20;
+            this.customerRA.TabStop = true;
+            this.customerRA.Text = "Customer";
+            this.customerRA.UseVisualStyleBackColor = true;
+            // 
+            // volunteerRA
+            // 
+            this.volunteerRA.AutoSize = true;
+            this.volunteerRA.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volunteerRA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.volunteerRA.Location = new System.Drawing.Point(190, 104);
+            this.volunteerRA.Margin = new System.Windows.Forms.Padding(2);
+            this.volunteerRA.Name = "volunteerRA";
+            this.volunteerRA.Size = new System.Drawing.Size(102, 24);
+            this.volunteerRA.TabIndex = 19;
+            this.volunteerRA.TabStop = true;
+            this.volunteerRA.Text = "Volunteer";
+            this.volunteerRA.UseVisualStyleBackColor = true;
+            // 
+            // employeesRA
+            // 
+            this.employeesRA.AutoSize = true;
+            this.employeesRA.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeesRA.Location = new System.Drawing.Point(84, 104);
+            this.employeesRA.Margin = new System.Windows.Forms.Padding(2);
+            this.employeesRA.Name = "employeesRA";
+            this.employeesRA.Size = new System.Drawing.Size(102, 24);
+            this.employeesRA.TabIndex = 18;
+            this.employeesRA.TabStop = true;
+            this.employeesRA.Text = "Employee";
+            this.employeesRA.UseVisualStyleBackColor = true;
+            this.employeesRA.Visible = false;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.Transparent;
+            this.searchBtn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBtn.Image = ((System.Drawing.Image)(resources.GetObject("searchBtn.Image")));
+            this.searchBtn.Location = new System.Drawing.Point(627, 46);
+            this.searchBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(42, 32);
+            this.searchBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.searchBtn.TabIndex = 11;
+            this.searchBtn.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(23, 95);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(139)))), ((int)(((byte)(139)))));
+            this.label2.Location = new System.Drawing.Point(340, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Search :";
+            // 
+            // searchTB
+            // 
+            this.searchTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.searchTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchTB.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.searchTB.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.searchTB.Location = new System.Drawing.Point(337, 46);
+            this.searchTB.Margin = new System.Windows.Forms.Padding(2);
+            this.searchTB.Name = "searchTB";
+            this.searchTB.Size = new System.Drawing.Size(286, 30);
+            this.searchTB.TabIndex = 9;
+            this.searchTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // formManageAccount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(85)))), ((int)(((byte)(58)))));
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "formManageAccount";
             this.Size = new System.Drawing.Size(1050, 643);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Addpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label addCustomerBtn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label addVolunteerBtn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label addEmployeesBtn;
+        private System.Windows.Forms.PictureBox Addpic;
+        private System.Windows.Forms.RadioButton customerRA;
+        private System.Windows.Forms.RadioButton volunteerRA;
+        private System.Windows.Forms.RadioButton employeesRA;
+        private System.Windows.Forms.PictureBox searchBtn;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchTB;
     }
 }
