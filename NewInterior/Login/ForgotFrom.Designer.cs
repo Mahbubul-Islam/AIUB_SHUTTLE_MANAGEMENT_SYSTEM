48c790a730b553cd7c2632be0d23864c4fcd0fcd
Tapu Barua <tapubarua7446@gmail.com>
2025-01-30 15:06:41 +0600

    added file

--- NewInterior/Login/ForgotFrom.Designer.cs
+++ NewInterior/Login/ForgotFrom.Designer.cs
@@ -162,7 +162,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(606, 626);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
